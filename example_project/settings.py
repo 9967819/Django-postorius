db12f81675487c2bd4ac2ebc868de4b4290adeb2
Mark Sapiro <mark@msapiro.net>
2023-12-22 10:30:50 -0800

    Add django.contrib.humanize to INSTALLED_APPS.

--- example_project/settings.py
+++ example_project/settings.py
@@ -69,6 +69,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'postorius',
     'django_mailman3',
     'django_gravatar',
